4450bf2f7acf494c7c89dd0231c5c799bd7c11c5
Doodl0 <52604696+Doodl0@users.noreply.github.com>
2025-06-18 22:08:00 +0100

    Added TUI menu to select server files to download

--- src/download.rs
+++ src/download.rs
@@ -1,21 +1,24 @@
 use reqwest::blocking::Response;
+use std::collections::{VecDeque};
 use std::fs::File;
 use std::io;
 
-pub fn get_json_manifest(url: &str) -> serde_json::Value {
+const DEFAULT_MANIFEST: &str = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+
+fn get_json_manifest(url: &str) -> serde_json::Value {
     let manifest:Response = reqwest::blocking::get(url).unwrap();
 
     let manifest_json:serde_json::Value = serde_json::from_str(&manifest.text().unwrap()).unwrap();
     return manifest_json;
 }
 
-pub fn get_latest_version() -> String {
-    let manifest: serde_json::Value = get_json_manifest("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+fn get_latest_version(url: &str) -> String {
+    let manifest: serde_json::Value = get_json_manifest(url);
     return manifest["latest"]["release"].to_string().replace('"',"");
 }
 
-pub fn get_version_url(version: &str) -> Option<String> {
-    let manifest: serde_json::Value = get_json_manifest("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+fn get_version_url(version: &str, url: &str) -> Option<String> {
+    let manifest: serde_json::Value = get_json_manifest(url);
     let versions: Option<&Vec<serde_json::Value>> = manifest["versions"].as_array();
 
     for id in versions.unwrap() {
@@ -24,7 +27,6 @@ pub fn get_version_url(version: &str) -> Option<String> {
         if version == ver {
             
             let url: String = id["url"].to_string().replace('"', "");
-            println!("found {}", url);
             return Some(url);
         }
     }
@@ -32,9 +34,54 @@ pub fn get_version_url(version: &str) -> Option<String> {
     return None;
 }
 
-pub fn save_file(url: &str, filepath: &str) {
+fn save_file(url: &str, filepath: String) {
     let mut response = reqwest::blocking::get(url).unwrap();
     let mut file = File::create(filepath).unwrap();
     io::copy(&mut response, &mut file).unwrap();
-    print!("Saved file as {}", filepath);
 } 
+
+pub fn download_server_jar(version: Option<&str>, filepath: String, manifest: Option<&str>) {
+    let url: &str;
+    let ver: String;
+    match manifest {
+        Some(t) => url = t,
+        None => url = DEFAULT_MANIFEST
+    }
+    match version {
+        Some(t) => ver = t.to_string(),
+        None => ver = get_latest_version(url),
+    }
+
+    let version_url = get_version_url(&ver, url).unwrap().replace('"', "");
+
+    let download_link =
+        get_json_manifest(&version_url.as_str())["downloads"]["server"]["url"].to_string().replace('"', "");
+
+    save_file(&download_link, filepath);
+}
+
+pub fn get_version_list(manifest: Option<&str>) -> (VecDeque<String>, VecDeque<String>) {
+    let url: &str;
+    match manifest {
+        Some(t) => url = t,
+        None => url = DEFAULT_MANIFEST
+    }
+
+    let versions = &get_json_manifest(url)["versions"];
+    let versions = versions.as_array().unwrap();
+
+    let mut release_list= VecDeque::new();
+    let mut snapshot_list= VecDeque::new();
+
+    for version in versions{
+        let id: String = version["id"].to_string().replace('"', "");
+        let release_type: String = version["type"].to_string().replace('"', "");
+        match release_type.as_str(){
+            "release" => release_list.push_back(id),
+            "snapshot" => snapshot_list.push_back(id),
+            _ => ()
+        }
+    }
+
+    return (release_list, snapshot_list);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 mod download;
-use crate::download::*;
+use cursive::{views::*, Cursive, CursiveExt};
+
+use crate::download::{download_server_jar, get_version_list};
 
 fn main() {
-    let version_url = get_version_url(&get_latest_version()).unwrap().replace('"', "");
+    let mut siv = Cursive::default();
 
-    let download_link =
-        get_json_manifest(&version_url.as_str())["downloads"]["server"]["url"].to_string().replace('"', "");
-    println!("Found {}", download_link);
+    
+    let mut select_view = cursive::views::SelectView::new();
+    let (release_list, snapshot_list) = get_version_list(None);
 
-    save_file(&download_link, r#"server.jar"#);
+    let mut index = 0;
+    for ver in release_list.clone() {
+        select_view.add_item(ver, index);
+        index += 1;
+    }
+
+    select_view.set_on_submit(|_, item|{
+        let (release_list, _) = get_version_list(None);
+        download_server_jar(Some(&release_list[*item]), format!("{}.jar",&release_list[*item]), None);
+    });
+
+    let scroll_view = ScrollView::new(select_view);
+    let resized_view = ResizedView::with_full_width(scroll_view);
+
+    siv.add_layer(resized_view);
+    siv.run();
 }
